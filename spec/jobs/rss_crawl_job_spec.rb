be84da8f75edde6f4bee48504107d4f962b40833
tmknom <tmk.nom@gmail.com>
2016-01-01 21:45:45 +0900

    RssCrawlJobのテスト作成

--- spec/jobs/rss_crawl_job_spec.rb
+++ spec/jobs/rss_crawl_job_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe RssCrawlJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  describe '#perform_later' do
+    it 'キューにJobが正しく登録されること' do
+      assert_enqueued_with(job: RssCrawlJob, args: ['sample1'], queue: 'default') do
+        assert_no_enqueued_jobs
+        RssCrawlJob.perform_later 'sample1'
+        assert_enqueued_jobs 1
+      end
+    end
+  end
+
 end
