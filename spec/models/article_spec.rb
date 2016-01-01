67ac5d36a7c56499a2089af6b4fc786db3b17ba2
tmknom <tmk.nom@gmail.com>
2016-01-01 15:19:51 +0900

    articleモデルのテストをfactory_girlで書き換え

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,25 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe '参照系' do
-    before do
-      @article = Article.new(url: 'https://www.google.co.jp/', title: 'Google', description: '検索エンジンだよ', bookmark_date_time: '2015/12/30 12:34:56')
-      @article.save
-    end
+    let(:article) { FactoryGirl.create(:article) }
 
     it '#all' do
-      expect(Article.all[0].url).to eq @article.url
+      article # letだと遅延評価されるので一度呼んでおく
+      expect(Article.all[0].url).to eq article.url
     end
     it '#first' do
-      expect(Article.first.url).to eq @article.url
+      article # letだと遅延評価されるので一度呼んでおく
+      expect(Article.first.url).to eq article.url
     end
     it '#find_by' do
-      expect(Article.find_by(title: 'Google').url).to eq @article.url
+      expect(Article.find_by(title: article.title).url).to eq article.url
     end
     it '#where' do
-      expect(Article.where('title = ?', 'Google')[0].url).to eq @article.url
+      expect(Article.where('title = ?', article.title)[0].url).to eq article.url
     end
     it '#find' do
-      expect(Article.find(@article.id).url).to eq @article.url
+      expect(Article.find(article.id).url).to eq article.url
     end
   end
 
@@ -56,42 +55,39 @@ RSpec.describe Article, type: :model do
   end
 
   describe 'update系' do
-    before do
-      @article = Article.create(url: 'http://test.before.com/', title: 'Google', description: '検索エンジンだよ', bookmark_date_time: '2015/12/30 12:34:56')
-    end
+    let(:article) { FactoryGirl.create(:article) }
+
     it '#save' do
-      @article.url = 'http://test.save.com/'
-      @article.save
+      article.url = 'http://test.save.com/'
+      article.save
       expect(Article.first.url).to eq 'http://test.save.com/'
-      expect(Article.first.title).to eq 'Google'
+      expect(Article.first.title).to eq article.title
     end
     it '#save!' do
-      @article.url = 'http://test.save!.com/'
-      @article.save
+      article.url = 'http://test.save!.com/'
+      article.save!
       expect(Article.first.url).to eq 'http://test.save!.com/'
-      expect(Article.first.title).to eq 'Google'
+      expect(Article.first.title).to eq article.title
     end
     it '#update' do
-      @article.update(url: 'http://test.update.com/')
+      article.update(url: 'http://test.update.com/')
       expect(Article.first.url).to eq 'http://test.update.com/'
-      expect(Article.first.title).to eq 'Google'
+      expect(Article.first.title).to eq article.title
     end
     it '#update_attribute' do
-      @article.update_attribute(:url, 'http://test.update_attribute.com/')
+      article.update_attribute(:url, 'http://test.update_attribute.com/')
       expect(Article.first.url).to eq 'http://test.update_attribute.com/'
-      expect(Article.first.title).to eq 'Google'
+      expect(Article.first.title).to eq article.title
     end
   end
 
   describe 'delete系' do
-    before do
-      @article = Article.create(url: 'http://test.before.com/', title: 'Google', description: '検索エンジンだよ', bookmark_date_time: '2015/12/30 12:34:56')
-    end
+    let(:article) { FactoryGirl.create(:article) }
 
     it '#destroy' do
-      @article.destroy
+      article.destroy
       expect(Article.first).to eq nil
-      expect(@article.destroyed?).to be(true)
+      expect(article.destroyed?).to be(true)
     end
     it '#delete_all' do
       Article.delete_all
@@ -100,28 +96,27 @@ RSpec.describe Article, type: :model do
   end
 
   describe '異常系' do
-    before do
-      @article = Article.new(url: 'https://www.google.co.jp/', title: 'Google', description: '検索エンジンだよ', bookmark_date_time: '2015/12/30 12:34:56')
-    end
+    let(:article) { FactoryGirl.build(:article) }
+
     it 'url がnilの場合' do
-      @article.url = nil
-      expect { @article.save }.to raise_error(ActiveRecord::StatementInvalid)
+      article.url = nil
+      expect { article.save }.to raise_error(ActiveRecord::StatementInvalid)
     end
     it 'title がnilの場合' do
-      @article.title = nil
-      expect { @article.save }.to raise_error(ActiveRecord::StatementInvalid)
+      article.title = nil
+      expect { article.save }.to raise_error(ActiveRecord::StatementInvalid)
     end
     it 'description がnilの場合' do
-      @article.description = nil
-      expect { @article.save }.to raise_error(ActiveRecord::StatementInvalid)
+      article.description = nil
+      expect { article.save }.to raise_error(ActiveRecord::StatementInvalid)
     end
     it 'url がユニークじゃない場合' do
-      Article.create!(url: 'https://www.google.co.jp/', title: 'Google', description: '検索エンジンだよ', bookmark_date_time: '2015/12/30 12:34:56')
-      expect { @article.save }.to raise_error(ActiveRecord::RecordNotUnique)
+      Article.create!(url: article.url, title: 'Google', description: '検索エンジンだよ', bookmark_date_time: '2015/12/30 12:34:56')
+      expect { article.save }.to raise_error(ActiveRecord::RecordNotUnique)
     end
     it 'bookmark_date_time が日付じゃない場合' do
-      @article.bookmark_date_time = 'invalid'
-      expect { @article.save }.to raise_error(ActiveRecord::StatementInvalid)
+      article.bookmark_date_time = 'invalid'
+      expect { article.save }.to raise_error(ActiveRecord::StatementInvalid)
     end
   end
 
