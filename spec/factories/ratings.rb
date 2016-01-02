8f0da643989004ee00e41ebd5f8a9301e17b6101
tmknom <tmk.nom@gmail.com>
2016-01-03 01:37:00 +0900

    ratingsテーブルのスキーマ情報をコメントに追加 (bundle exec annotate)

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: ratings
+#
+#  id                    :integer          not null, primary key
+#  article_id            :integer          not null
+#  hatena_bookmark_count :integer          default(0), not null
+#  facebook_count        :integer          default(0), not null
+#  pocket_count          :integer          default(0), not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+# Indexes
+#
+#  index_ratings_on_article_id  (article_id)
+#
+# Foreign Keys
+#
+#  fk_rails_e25201a524  (article_id => articles.id)
+#
+
 FactoryGirl.define do
   factory :rating do
     article
