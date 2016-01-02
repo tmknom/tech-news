8b633af817dfd07a877e06db256dd26957247880
tmknom <tmk.nom@gmail.com>
2016-01-02 22:37:07 +0900

    ルーティング情報をコメントに追記 (bundle exec annotate --routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+# == Route Map
+#
+#       Prefix Verb   URI Pattern                  Controller#Action
+#     articles GET    /articles(.:format)          articles#index
+#              POST   /articles(.:format)          articles#create
+#  new_article GET    /articles/new(.:format)      articles#new
+# edit_article GET    /articles/:id/edit(.:format) articles#edit
+#      article GET    /articles/:id(.:format)      articles#show
+#              PATCH  /articles/:id(.:format)      articles#update
+#              PUT    /articles/:id(.:format)      articles#update
+#              DELETE /articles/:id(.:format)      articles#destroy
+#
+
 Rails.application.routes.draw do
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
