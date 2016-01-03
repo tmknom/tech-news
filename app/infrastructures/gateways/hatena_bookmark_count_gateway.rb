004bab1b1514e7f59403a50cdab0c396903ab300
tmknom <tmk.nom@gmail.com>
2016-01-03 14:15:49 +0900

    HatenaBookmarkCountGatewayを追加

---
+++ app/infrastructures/gateways/hatena_bookmark_count_gateway.rb
@@ -0,0 +1,10 @@
+require 'open-uri'
+
+class HatenaBookmarkCountGateway
+  HATENA_BOOKMARK_COUNT_URL = 'http://api.b.st-hatena.com/entry.count?url='.freeze
+
+  def get(url)
+    raw_body = open(HATENA_BOOKMARK_COUNT_URL + url).read
+    raw_body.empty? ? '0' : raw_body
+  end
+end
