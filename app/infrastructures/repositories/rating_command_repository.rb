037b388717547dd0861cb8fccb2d5ae8072da6ec
tmknom <tmk.nom@gmail.com>
2016-01-03 13:37:05 +0900

    RatingCommandRepositoryにはてブ数を保存するメソッドを追加

--- app/infrastructures/repositories/rating_command_repository.rb
+++ app/infrastructures/repositories/rating_command_repository.rb
@@ -4,4 +4,9 @@ class RatingCommandRepository
       Rating.create(article_id: article_id, hatena_bookmark_count: 0, facebook_count: 0, pocket_count: 0)
     end
   end
+
+  def save_hatena_bookmark_count(article_id, hatena_bookmark_count)
+    rating = Rating.find_by(article_id: article_id)
+    rating.update(hatena_bookmark_count: hatena_bookmark_count)
+  end
 end
