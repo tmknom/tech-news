f25eafd08750e467a15ececa9a6a6010531e4d31
tmknom <tmk.nom@gmail.com>
2016-01-03 18:24:27 +0900

    FacebookCountGatewayの実装

---
+++ app/infrastructures/gateways/facebook_count_gateway.rb
@@ -0,0 +1,11 @@
+class FacebookCountGateway
+  require 'json'
+
+  FACEBOOK_COUNT_URL = 'http://graph.facebook.com/?id='.freeze
+
+  def get(url)
+    raw_body = open(FACEBOOK_COUNT_URL + url).read
+    json = JSON.parse(raw_body)
+    json.has_key?('shares') ? json['shares'] : 0
+  end
+end
