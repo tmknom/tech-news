29681b2a09c8a7b2a8819dafc1852603c078542e
tmknom <tmk.nom@gmail.com>
2016-01-04 17:33:12 +0900

    スタブ化できるようHatenaBookmarkCountCrawlApplicationのGateway実行部分をプライベートメソッド化

--- app/applications/hatena_bookmark_count_crawl_application.rb
+++ app/applications/hatena_bookmark_count_crawl_application.rb
@@ -7,7 +7,13 @@ class HatenaBookmarkCountCrawlApplication
 
   def crawl(url)
     article_id = @article_query_repository.get_id_by_url url
-    hatena_bookmark_count = @hatena_bookmark_count_gateway.get url
+    hatena_bookmark_count = get_hatena_bookmark_count url
     @rating_command_repository.save_hatena_bookmark_count(article_id, hatena_bookmark_count)
+  end
+
+  private
+
+  def get_hatena_bookmark_count(url)
+    @hatena_bookmark_count_gateway.get url
   end
 end
