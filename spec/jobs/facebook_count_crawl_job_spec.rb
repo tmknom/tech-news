3fee2254936bc3852a0e9dc13799f2696696b3e5
tmknom <tmk.nom@gmail.com>
2016-01-05 16:30:35 +0900

    rating系Jobクラスのテストのリファクタ

--- spec/jobs/facebook_count_crawl_job_spec.rb
+++ spec/jobs/facebook_count_crawl_job_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe FacebookCountCrawlJob, type: :job do
 
   describe '#perform_later' do
     it 'キューにJobが正しく登録されること' do
-      assert_enqueued_with(job: FacebookCountCrawlJob, args: ['dummy_url'], queue: 'default') do
+      assert_enqueued_with(job: FacebookCountCrawlJob, args: ['dummy_id'], queue: 'default') do
         # キューにジョブが登録されていないことを確認
         assert_no_enqueued_jobs
         # 実行
-        FacebookCountCrawlJob.perform_later 'dummy_url'
+        FacebookCountCrawlJob.perform_later 'dummy_id'
         # キューにジョブが登録されたことを確認
         assert_enqueued_jobs 1
       end
--- spec/jobs/hatena_bookmark_count_crawl_job_spec.rb
+++ spec/jobs/hatena_bookmark_count_crawl_job_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe HatenaBookmarkCountCrawlJob, type: :job do
 
   describe '#perform_later' do
     it 'キューにJobが正しく登録されること' do
-      assert_enqueued_with(job: HatenaBookmarkCountCrawlJob, args: ['dummy_url'], queue: 'default') do
+      assert_enqueued_with(job: HatenaBookmarkCountCrawlJob, args: ['dummy_id'], queue: 'default') do
         # キューにジョブが登録されていないことを確認
         assert_no_enqueued_jobs
         # 実行
-        HatenaBookmarkCountCrawlJob.perform_later 'dummy_url'
+        HatenaBookmarkCountCrawlJob.perform_later 'dummy_id'
         # キューにジョブが登録されたことを確認
         assert_enqueued_jobs 1
       end
