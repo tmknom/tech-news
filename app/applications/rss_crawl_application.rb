007666f6755c4e56b020b3d72892a9fe31933b68
tmknom <tmk.nom@gmail.com>
2016-01-05 11:57:07 +0900

    RssCrawlApplicationで記事保存処理で例外吐いても処理を継続する

--- app/applications/rss_crawl_application.rb
+++ app/applications/rss_crawl_application.rb
@@ -9,9 +9,21 @@ class RssCrawlApplication
   def crawl(url)
     rss_items = @rss_gateway.get url
     rss_items.each do |rss_item|
+      save_rss_item rss_item
+    end
+  end
+
+  private
+
+  def save_rss_item(rss_item)
+    begin
       article = @article_rss_converter.convert rss_item
       @article_command_repository.save_if_not_exists article
       @rating_command_repository.save_if_not_exists article.id
+    rescue => e
+      # todo ログ吐いたほうがいい
+      # 例外が発生しても次の記事はそのまま処理して欲しいので、とりあえず標準出力して、処理を継続する
+      p e.message
     end
   end
 end
