f1eb2e6105c24bbb3cf1ae88f4873fea80e8318b
tmknom <tmk.nom@gmail.com>
2016-01-05 16:27:37 +0900

    RssCrawlJobのperformメソッドが引数urlを取るよう変更

--- app/jobs/rss_crawl_job.rb
+++ app/jobs/rss_crawl_job.rb
@@ -1,12 +1,7 @@
 class RssCrawlJob < ActiveJob::Base
   queue_as :default
 
-  HATENA_BOOKMARK_RSS_URL = 'http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss'.freeze
-
-  # RssCrawlJob.perform_later
-  def perform
-    p "start #{self.class}"
-    RssCrawlApplication.new.crawl HATENA_BOOKMARK_RSS_URL
-    p "end #{self.class}"
+  def perform(url)
+    RssCrawlApplication.new.crawl url
   end
 end
