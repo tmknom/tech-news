c23097e06e6698e8dc3c61db6a1f11d73d78d349
tmknom <tmk.nom@gmail.com>
2016-01-05 15:15:32 +0900

    Task関連のテストにコメントを追記

--- spec/tasks/facebook_count_crawl_task_spec.rb
+++ spec/tasks/facebook_count_crawl_task_spec.rb
@@ -9,8 +9,11 @@ RSpec.describe FacebookCountCrawlTask, type: :task do
 
   describe '#run' do
     it 'FacebookCountCrawlJobが3件キューに登録されること' do
+      # キューにジョブが登録されていないことを確認
       assert_no_enqueued_jobs
+      # 実行
       FacebookCountCrawlTask.new.run
+      # キューにジョブが登録されたことを確認
       assert_enqueued_jobs 3
       expect(enqueued_jobs.first[:job]).to eq FacebookCountCrawlJob
     end
