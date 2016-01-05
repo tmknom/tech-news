3dbae8c16d75fa64f0676210a2c6436ec20efb33
tmknom <tmk.nom@gmail.com>
2016-01-05 15:11:05 +0900

    FacebookCountCrawlJobのテストをリファクタ

--- spec/jobs/facebook_count_crawl_job_spec.rb
+++ spec/jobs/facebook_count_crawl_job_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe FacebookCountCrawlJob, type: :job do
 
   describe '#perform_later' do
     it 'キューにJobが正しく登録されること' do
-      assert_enqueued_with(job: FacebookCountCrawlJob, args: [], queue: 'default') do
+      assert_enqueued_with(job: FacebookCountCrawlJob, args: ['dummy_url'], queue: 'default') do
         assert_no_enqueued_jobs
-        FacebookCountCrawlJob.perform_later
+        FacebookCountCrawlJob.perform_later 'dummy_url'
         assert_enqueued_jobs 1
       end
     end
