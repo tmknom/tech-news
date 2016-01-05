b30c89de9453ccadf2124b0351cc0bc44498ca80
tmknom <tmk.nom@gmail.com>
2016-01-05 16:24:21 +0900

    RSS取得関連のクラスのテストリファクタ

--- spec/applications/rss_crawl_application_spec.rb
+++ spec/applications/rss_crawl_application_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe RssCrawlApplication, type: :application do
   let(:rss_crawl_application) { RssCrawlApplication.new }
 
-  HATENA_BOOKMARK_RSS_URL = 'http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss'.freeze
 
   describe '#crawl' do
     it '正常系' do
@@ -11,8 +10,9 @@ RSpec.describe RssCrawlApplication, type: :application do
       expect(Article.all.size).to eq 0
       expect(Rating.all.size).to eq 0
       # 実行
+      url = 'http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss'
       VCR.use_cassette 'rss/b.hatena.ne.jp' do
-        rss_crawl_application.crawl HATENA_BOOKMARK_RSS_URL
+        rss_crawl_application.crawl url
       end
       # クロール後にデータが登録されたことを確認
       expect(Article.all.size).to eq 30
--- spec/infrastructures/gateways/rss_gateway_spec.rb
+++ spec/infrastructures/gateways/rss_gateway_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe RssGateway, type: :model do
 
-  HATENA_BOOKMARK_RSS_URL = 'http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss'.freeze
-
   describe '#get' do
     let!(:rss_gateway) { RssGateway.new }
 
     it '正常系' do
+      url = 'http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss'
       VCR.use_cassette 'rss/b.hatena.ne.jp' do
-        rss_items = rss_gateway.get HATENA_BOOKMARK_RSS_URL
+        rss_items = rss_gateway.get url
         expect(rss_items.size).to eq 30
         expect(rss_items[0].title.force_encoding('utf-8')).to eq 'ウェブ関係者よ、PVの話をするのはもう止めよう « WIRED.jp'
         expect(rss_items[0].link.force_encoding('utf-8')).to eq 'http://wired.jp/2016/01/03/page-views-dont-matter/'
