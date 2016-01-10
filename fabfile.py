c9dac343d97bb002a1cf9e9f49543e244ca4f3d8
tmknom <tmk.nom@gmail.com>
2016-01-11 02:30:04 +0900

    デプロイ時に最新のシンボリックリンクを貼るよう修正

--- fabfile.py
+++ fabfile.py
@@ -4,24 +4,37 @@ from datetime import datetime
 
 REPOSITORY = 'tmknom/tech-news'
 WORK_DIR = datetime.now().strftime("%Y%m%d_%H%M%S")
+RELEASES_DIR = 'releases'
+CURRENT_DIR = 'current'
 
 @task
 def deploy():
   execute_deploy()
 
 def execute_deploy():
+  initialize_dir()
   clone()
   bundle_install()
   db_migrate()
+  symlink()
+
+def initialize_dir():
+  env.release_dir = RELEASES_DIR + '/' + WORK_DIR
+  run('mkdir -p %s' % (RELEASES_DIR))
 
 def clone():
   repository_url = 'https://github.com/' + REPOSITORY + '.git'
-  run('git clone %s %s' %(repository_url, WORK_DIR))
+  with cd(RELEASES_DIR):
+    run('git clone %s %s' %(repository_url, WORK_DIR))
 
 def bundle_install():
-  with cd(WORK_DIR):
+  with cd(env.release_dir):
     run('bundle install --path vendor/bundle --without development')
 
 def db_migrate():
-  with cd(WORK_DIR):
+  with cd(env.release_dir):
     run('RAILS_ENV=production bundle exec rake db:migrate')
+
+def symlink():
+  run('ln -snf %s %s' % (env.release_dir, CURRENT_DIR))
+
