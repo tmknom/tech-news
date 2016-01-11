dd3b62769cd8db9862ec46219e83256cec0bbc73
tmknom <tmk.nom@gmail.com>
2016-01-11 16:14:04 +0900

    ArticlesControllerのindexメソッドを修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    article = Article.new(url: 'http://test.save.com/', title: 'Google', description: '検索エンジンだよ', bookmarked_at: '2015/12/30 12:34:56')
-    render json: article
+    articles = ArticleQueryRepository.new.list_week
+    render json: articles
   end
 end
