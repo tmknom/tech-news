c21197aa25574c60eef87d579f0d261078f91675
tmknom <tmk.nom@gmail.com>
2016-01-11 16:10:51 +0900

    ArticleQueryRepositoryにlist_todayとlist_weekメソッド作成

--- app/infrastructures/repositories/article_query_repository.rb
+++ app/infrastructures/repositories/article_query_repository.rb
@@ -3,6 +3,14 @@ class ArticleQueryRepository
     Article.find(id)
   end
 
+  def list_today
+    Article.where('created_at > (CURDATE() - INTERVAL 1 DAY)').order(bookmarked_at: :desc).limit(30)
+  end
+
+  def list_week
+    Article.where('created_at > (CURDATE() - INTERVAL 7 DAY)').order(bookmarked_at: :desc).limit(30)
+  end
+
   def list_recent_id
     Article.order(bookmarked_at: :desc).limit(30).ids
   end
