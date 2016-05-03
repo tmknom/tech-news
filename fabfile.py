082165f2fafc968fe85fb303852560139d1e1228
tmknom <tmk.nom@gmail.com>
2016-05-03 19:59:31 +0900

    CodeDeployのデプロイ実行タスク作成

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,55 @@
 # -*- encoding:utf-8 -*-
 
 from fabric.api import *
-from datetime import datetime
 
+APPLICATION_NAME = 'tech-news'
 REPOSITORY = 'tmknom/tech-news'
-WORK_DIR = datetime.now().strftime("%Y%m%d_%H%M%S")
-RELEASES_DIR = 'releases'
-CURRENT_DIR = 'current'
+
+ADMINISTRATION = 'Administration'
+PRODUCTION = 'Production'
+
+
+@task
+def deploy_production(branch='master'):
+    deploy(branch, PRODUCTION)
+
+
+@task
+def deploy_administration(branch='master'):
+    deploy(branch, ADMINISTRATION)
+
+
+def deploy(branch, environment):
+    recent_commit_id = get_recent_commit_id(branch)
+    create_deployment(recent_commit_id, environment)
+
+
+def create_deployment(commit_id, environment):
+    region = get_default_region()
+    deployment_group_name = environment + '-' + APPLICATION_NAME
+    execute_create_deployment(commit_id, deployment_group_name, region, APPLICATION_NAME, REPOSITORY)
+
+
+def execute_create_deployment(commit_id, deployment_group_name, region, application_name, repository):
+    command = "aws deploy create-deployment " \
+              + " --region %s " % (region) \
+              + " --application-name %s " % (application_name.lower()) \
+              + " --deployment-group-name %s " % (deployment_group_name.lower()) \
+              + " --github-location commitId=%s,repository=%s " % (commit_id, repository)
+    result = local(command, capture=True)
+    return result
+
+
+def get_default_region():
+    command = 'aws configure get region'
+    result = local(command, capture=True)
+    return result
+
+
+def get_recent_commit_id(branch):
+    command = 'git log -n 1 --format=%H ' + branch
+    result = local(command, capture=True)
+    return result
 
 
 @task
