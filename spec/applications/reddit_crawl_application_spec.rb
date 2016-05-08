3b84cb2e843226b4a2416a846a1da2e43506d845
tmknom <tmk.nom@gmail.com>
2016-05-08 18:11:27 +0900

    RedditCrawlApplicationの雛形作成

---
+++ spec/applications/reddit_crawl_application_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe RedditCrawlApplication, type: :application do
+  let(:rss_crawl_application) { RedditCrawlApplication.new }
+
+
+  describe '#crawl' do
+    it '正常系' do
+      puts 'test later : RedditCrawlApplication'
+    end
+  end
+
+end
