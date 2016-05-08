f65f10aa737bdd9a9118d312095d5274424e49a9
tmknom <tmk.nom@gmail.com>
2016-05-09 08:35:25 +0900

    reddit_articlesテーブルのスキーマ定義（bundle exec rake db:migrate:reset）

--- app/models/reddit_article.rb
+++ app/models/reddit_article.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: reddit_articles
+#
+#  id          :integer          not null, primary key
+#  url         :string(255)      not null
+#  title       :string(255)      not null
+#  image_url   :string(255)      not null
+#  description :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_reddit_articles_on_url  (url) UNIQUE
+#
+
 class RedditArticle < ActiveRecord::Base
 end
