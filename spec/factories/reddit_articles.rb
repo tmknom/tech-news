a3339848dd172fede3f8eef34775fcb841357d72
tmknom <tmk.nom@gmail.com>
2016-05-09 08:57:23 +0900

    reddit_articlesテーブルのスキーマ情報をコメントに追加 (bundle exec annotate)

--- spec/factories/reddit_articles.rb
+++ spec/factories/reddit_articles.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: reddit_articles
+#
+#  id          :integer          not null, primary key
+#  url         :string(255)      not null
+#  title       :string(255)      not null
+#  image_url   :string(255)      not null
+#  description :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_reddit_articles_on_url  (url) UNIQUE
+#
+
 FactoryGirl.define do
   factory :reddit_article do
     sequence(:url) { |i| "https://www.google#{i}.co.jp/" }
