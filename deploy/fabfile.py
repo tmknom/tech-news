3fca988051f1766b199712747c359dc0ea96ee71
tmknom <tmk.nom@gmail.com>
2016-05-08 14:50:19 +0900

    デプロイ時に.bashrcを読み込むよう修正

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -85,7 +85,7 @@ def validate_service():
 
 
 def bundle(bundle_option):
-    command = 'source %s/.bash_profile && RAILS_ENV=production /opt/.rbenv/shims/bundle %s' % (HOME_DIR, bundle_option)
+    command = 'source %s/.bashrc && RAILS_ENV=production /opt/.rbenv/shims/bundle %s' % (HOME_DIR, bundle_option)
     local_su(command, CURRENT_DIR)
 
 
