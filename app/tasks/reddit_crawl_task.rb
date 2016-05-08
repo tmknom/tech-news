785d0290ea677612364011014f98bf2597bfa9f3
tmknom <tmk.nom@gmail.com>
2016-05-08 15:30:44 +0900

    RedditCrawlTaskの作成

---
+++ app/tasks/reddit_crawl_task.rb
@@ -0,0 +1,9 @@
+class RedditCrawlTask
+  def run
+    RedditCrawlJob.perform_later RssUrl::REDDIT_GIFS
+  end
+end
+
+module RssUrl
+  REDDIT_GIFS = 'https://www.reddit.com/r/gifs/top/.rss'.freeze
+end
