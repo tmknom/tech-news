c3441caa4152ad6b33e66b74ab799a1aaafcdf62
tmknom <tmk.nom@gmail.com>
2016-05-10 11:51:24 +0900

    RedditRssTransformation作成

---
+++ app/infrastructures/transformations/reddit/reddit_rss_transformation.rb
@@ -0,0 +1,12 @@
+module Reddit
+  class RedditRssTransformation
+    def transform(rss_item)
+      title = rss_item.title.force_encoding('utf-8')
+      url = rss_item.link.force_encoding('utf-8')
+      media_url = 'dummy.gif'
+      description = CGI.unescapeHTML(rss_item.content.force_encoding('utf-8'))[0, 255]
+      posted_at = rss_item.updated.in_time_zone('Tokyo') # Time型
+      RedditArticle.new(url: url, title: title, media_url: media_url, description: description, posted_at: posted_at)
+    end
+  end
+end
