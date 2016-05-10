75194285ca6d5146b0500dbaf6acdd908d88a9fd
tmknom <tmk.nom@gmail.com>
2016-05-10 13:05:10 +0900

    Job系クラスでperformメソッドをテスト

--- spec/jobs/reddit_crawl_job_spec.rb
+++ spec/jobs/reddit_crawl_job_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RedditCrawlJob, type: :job do
   include ActiveJob::TestHelper
 
-  describe '#perform_later' do
+  describe '#perform' do
     it 'キューにJobが正しく登録されること' do
       assert_enqueued_with(job: RedditCrawlJob, args: ['dummy_url'], queue: 'rss') do
         # キューにジョブが登録されていないことを確認
@@ -14,6 +14,14 @@ RSpec.describe RedditCrawlJob, type: :job do
         assert_enqueued_jobs 1
       end
     end
-  end
 
+    it 'アプリケーションが例外をスローしないこと' do
+      url = 'https://www.reddit.com/r/gifs/hot/.rss'
+      VCR.use_cassette 'rss/www.reddit.com.gif' do
+        expect {
+          RedditCrawlJob.perform_now url
+        }.not_to raise_error
+      end
+    end
+  end
 end
--- spec/jobs/rss_crawl_job_spec.rb
+++ spec/jobs/rss_crawl_job_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RssCrawlJob, type: :job do
   include ActiveJob::TestHelper
 
-  describe '#perform_later' do
+  describe '#perform' do
     it 'キューにJobが正しく登録されること' do
       assert_enqueued_with(job: RssCrawlJob, args: ['dummy_url'], queue: 'rss') do
         # キューにジョブが登録されていないことを確認
@@ -12,6 +12,15 @@ RSpec.describe RssCrawlJob, type: :job do
         RssCrawlJob.perform_later 'dummy_url'
         # キューにジョブが登録されたことを確認
         assert_enqueued_jobs 1
+      end
+    end
+
+    it 'アプリケーションが例外をスローしないこと' do
+      url = 'http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss'
+      VCR.use_cassette 'rss/b.hatena.ne.jp' do
+        expect {
+          RssCrawlJob.perform_now url
+        }.not_to raise_error
       end
     end
   end
