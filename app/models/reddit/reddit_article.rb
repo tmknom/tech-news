1509a793dd07cf8b18ba09024e98806c9e9ea0f6
tmknom <tmk.nom@gmail.com>
2016-05-11 19:12:10 +0900

    reddit_articlesテーブルからmedia_urlとdescription削除

--- app/models/reddit/reddit_article.rb
+++ app/models/reddit/reddit_article.rb
@@ -2,19 +2,16 @@
 #
 # Table name: reddit_articles
 #
-#  id          :integer          not null, primary key
-#  url         :string(255)      not null
-#  title       :string(255)      not null
-#  media_url   :string(255)      not null
-#  description :string(255)      not null
-#  posted_at   :datetime         not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id         :integer          not null, primary key
+#  url        :string(255)      not null
+#  title      :string(255)      not null
+#  posted_at  :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 # Indexes
 #
-#  index_reddit_articles_on_media_url  (media_url) UNIQUE
-#  index_reddit_articles_on_url        (url) UNIQUE
+#  index_reddit_articles_on_url  (url) UNIQUE
 #
 
 module Reddit
