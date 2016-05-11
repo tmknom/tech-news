db4ee451e62c5b828866518fab15884b2d1af125
tmknom <tmk.nom@gmail.com>
2016-05-11 16:58:25 +0900

    media_urlを取得する正規表現を実装

--- app/infrastructures/transformations/reddit/reddit_rss_transformation.rb
+++ app/infrastructures/transformations/reddit/reddit_rss_transformation.rb
@@ -3,10 +3,19 @@ module Reddit
     def transform(rss_item)
       title = rss_item.title.force_encoding('utf-8')
       url = rss_item.link.force_encoding('utf-8')
-      media_url = url + 'dummy.gif'
-      description = CGI.unescapeHTML(rss_item.content.force_encoding('utf-8'))[0, 255]
       posted_at = rss_item.updated # Time型
+
+      content = rss_item.content.force_encoding('utf-8')
+      media_url = parse_media_url(content)
+      description = CGI.unescapeHTML(content)[0, 255]
+
       RedditArticle.new(url: url, title: title, media_url: media_url, description: description, posted_at: posted_at)
+    end
+
+    private
+
+    def parse_media_url(content)
+      content.match(%r{&lt;span&gt;&lt;a\shref=&quot;(.+?)&quot;&gt;\[link\]})[1]
     end
   end
 end
