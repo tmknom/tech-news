4be2902ec236b5350b89f39e359aee9a14e7dacf
tmknom <tmk.nom@gmail.com>
2016-05-14 18:41:37 +0900

    bundle exec rails gでテンプレートエンジンをerbを生成

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module TechNews
       g.stylesheets false
       g.javascripts false
       g.helper false
-      g.template_engine false
+      g.template_engine :erb
       g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
