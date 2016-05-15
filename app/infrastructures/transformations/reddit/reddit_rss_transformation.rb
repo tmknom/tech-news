f1c313db47a0fed011ee7e75ea3dbd7b126e2db4
tmknom <tmk.nom@gmail.com>
2016-05-15 12:30:09 +0900

    RedditRssTransformationで各要素の最大長を指定し、それ以上の長さの場合ぶった切る

--- app/infrastructures/transformations/reddit/reddit_rss_transformation.rb
+++ app/infrastructures/transformations/reddit/reddit_rss_transformation.rb
@@ -18,19 +18,22 @@ module Reddit
 
     def medium_url(rss_item)
       content = rss_item.content.force_encoding('utf-8')
-      content.match(%r{&lt;span&gt;&lt;a\shref=&quot;(.+?)&quot;&gt;\[link\]})[1]
+      medium_url = content.match(%r{&lt;span&gt;&lt;a\shref=&quot;(.+?)&quot;&gt;\[link\]})[1]
+      medium_url[0, MAX_MYSQL_RECORD_SIZE]
     end
 
     def reddit_url(rss_item)
-      rss_item.link.force_encoding('utf-8')
+      rss_item.link.force_encoding('utf-8')[0, MAX_MYSQL_RECORD_SIZE]
     end
 
     def title(rss_item)
-      rss_item.title.force_encoding('utf-8')
+      rss_item.title.force_encoding('utf-8')[0, MAX_MYSQL_RECORD_SIZE]
     end
 
     def posted_at(rss_item)
       rss_item.updated # Time型
     end
+
+    MAX_MYSQL_RECORD_SIZE = 255
   end
 end
