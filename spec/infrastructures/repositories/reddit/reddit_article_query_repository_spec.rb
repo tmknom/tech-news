bbd14f5b1944047a412371e985aae075fa822da1
tmknom <tmk.nom@gmail.com>
2016-05-15 15:20:33 +0900

    RedditArticleQueryRepositoryのlist_todayメソッドをlist_recentlyにリネーム＆where句変更

--- spec/infrastructures/repositories/reddit/reddit_article_query_repository_spec.rb
+++ spec/infrastructures/repositories/reddit/reddit_article_query_repository_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Reddit::RedditArticleQueryRepository, type: :model do
 
   let(:reddit_article_query_repository) { Reddit::RedditArticleQueryRepository.new }
 
-  describe '#list_today' do
+  describe '#list_recently' do
     let!(:reddit_medium) do
       create_list(:reddit_medium, 3)
     end
 
-    it '今日の一覧が取得できること' do
-      articles = reddit_article_query_repository.list_today
+    it '一覧が取得できること' do
+      articles = reddit_article_query_repository.list_recently nil
       expect(articles.size).to eq 3
     end
   end
