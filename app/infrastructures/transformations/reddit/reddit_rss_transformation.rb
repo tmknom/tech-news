b91f4dacb6b9b3326638d6085cc35c015229bb38
tmknom <tmk.nom@gmail.com>
2016-05-15 12:18:24 +0900

    RedditRssTransformationのリファクタ

--- app/infrastructures/transformations/reddit/reddit_rss_transformation.rb
+++ app/infrastructures/transformations/reddit/reddit_rss_transformation.rb
@@ -1,28 +1,36 @@
 module Reddit
   class RedditRssTransformation
     def transform(rss_item, category)
-      title = rss_item.title.force_encoding('utf-8')
+      title = title(rss_item)
       url = reddit_url(rss_item)
-      posted_at = rss_item.updated # Time型
+      posted_at = posted_at(rss_item)
 
       RedditArticle.new(url: url, title: title, category: category, posted_at: posted_at)
     end
 
     def transform_medium(reddit_article_id, rss_item)
-      content = rss_item.content.force_encoding('utf-8')
-      url = medium_url(content)
+      url = medium_url(rss_item)
 
       RedditMedium.new(reddit_article_id: reddit_article_id, url: url, media_type: RedditMedium::TYPE_IMAGE)
     end
 
     private
 
-    def medium_url(content)
+    def medium_url(rss_item)
+      content = rss_item.content.force_encoding('utf-8')
       content.match(%r{&lt;span&gt;&lt;a\shref=&quot;(.+?)&quot;&gt;\[link\]})[1]
     end
 
     def reddit_url(rss_item)
       rss_item.link.force_encoding('utf-8')
+    end
+
+    def title(rss_item)
+      rss_item.title.force_encoding('utf-8')
+    end
+
+    def posted_at(rss_item)
+      rss_item.updated # Time型
     end
   end
 end
