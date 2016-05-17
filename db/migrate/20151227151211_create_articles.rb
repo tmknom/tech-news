42153194091d221aed6fbdbc05c64a068046233d
tmknom <tmk.nom@gmail.com>
2016-05-17 16:27:07 +0900

    DBのテーブル定義から不要なオプション削除

--- db/migrate/20151227151211_create_articles.rb
+++ db/migrate/20151227151211_create_articles.rb
@@ -1,6 +1,6 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-    create_table :articles, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci' do |t|
+    create_table :articles do |t|
       t.string :url, null: false
       t.string :title, null: false
       t.string :description, null: false
--- db/migrate/20160102153034_create_ratings.rb
+++ db/migrate/20160102153034_create_ratings.rb
@@ -1,6 +1,6 @@
 class CreateRatings < ActiveRecord::Migration
   def change
-    create_table :ratings, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci' do |t|
+    create_table :ratings do |t|
       t.references :article, null: false, index: true, foreign_key: true
       t.integer :hatena_bookmark_count, null: false, default: 0
       t.integer :facebook_count, null: false, default: 0
