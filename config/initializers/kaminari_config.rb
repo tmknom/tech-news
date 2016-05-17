6e051d2471f78f9742b94d1d93f5b3211172cdd8
tmknom <tmk.nom@gmail.com>
2016-05-17 12:40:21 +0900

    kaminariの設定を変更

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,8 @@
 Kaminari.configure do |config|
-  # config.default_per_page = 25
+  config.default_per_page = 30
   # config.max_per_page = nil
-  # config.window = 4
-  # config.outer_window = 0
+  config.window = 4
+  config.outer_window = 1
   # config.left = 0
   # config.right = 0
   # config.page_method_name = :page
