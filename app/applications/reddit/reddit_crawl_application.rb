bed3653f4c4a7ba0ea6dff84ecf47fba4bba6f3a
tmknom <tmk.nom@gmail.com>
2016-05-19 21:07:29 +0900

    RedditCrawlApplicationをAPI経由でデータ取得するよう変更

--- app/applications/reddit/reddit_crawl_application.rb
+++ app/applications/reddit/reddit_crawl_application.rb
@@ -1,33 +1,28 @@
 module Reddit
   class RedditCrawlApplication
     def initialize
-      @rss_gateway = Rss::RssGateway.new
-      @reddit_rss_transformation = RedditRssTransformation.new
+      @reddit_extraction = RedditExtraction.new
+      @reddit_api_transformation = RedditApiTransformation.new
       @reddit_article_command_repository = RedditArticleCommandRepository.new
       @reddit_medium_command_repository = RedditMediumCommandRepository.new
     end
 
     def crawl(category)
-      rss_url = rss_url category
-      rss_items = @rss_gateway.get rss_url
-      rss_items.each do |rss_item|
-        save_rss_item rss_item, category
+      items = @reddit_extraction.extract category
+      items.each do |item|
+        save_rss_item item, category
       end
     end
 
     private
 
-    def rss_url(category)
-      "https://www.reddit.com/r/#{category}/hot/.rss".freeze
-    end
-
-    def save_rss_item(rss_item, category)
+    def save_rss_item(item, category)
       begin
-        reddit_article = @reddit_rss_transformation.transform rss_item, category
+        reddit_article = @reddit_api_transformation.transform item, category
         @reddit_article_command_repository.save_if_not_exists reddit_article
 
         unless reddit_article.id.nil?
-          reddit_medium = @reddit_rss_transformation.transform_medium reddit_article.id, rss_item
+          reddit_medium = @reddit_api_transformation.transform_medium reddit_article.id, item
           @reddit_medium_command_repository.save_if_not_exists reddit_medium
         end
       rescue => e
