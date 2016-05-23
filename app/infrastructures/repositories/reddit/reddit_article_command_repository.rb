decbacb821c3990751b89799a2fdd98803a38347
tmknom <tmk.nom@gmail.com>
2016-05-23 16:11:53 +0900

    scoreとcomment_countを毎回更新するよう修正

--- app/infrastructures/repositories/reddit/reddit_article_command_repository.rb
+++ app/infrastructures/repositories/reddit/reddit_article_command_repository.rb
@@ -1,8 +1,11 @@
 module Reddit
   class RedditArticleCommandRepository
     def save_if_not_exists(reddit_article)
-      unless RedditArticle.exists?(url: reddit_article.url)
+      selected_reddit_article = RedditArticle.find_by(url: reddit_article.url)
+      if selected_reddit_article.nil?
         reddit_article.save
+      else
+        selected_reddit_article.update(score: reddit_article.score, comment_count: reddit_article.comment_count)
       end
     end
   end
